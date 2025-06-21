512171c0adee8df38fa4b4141f1fdb6cfeb62d98
ravi9879 <ravigupta77389@gmail.com>
2025-06-21 18:08:31 +0530

    updated

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cp());
 // app.use(cors());
 app.use(cors({
         origin: "http://localhost:3000",
-        // preflightContinue : true ,
+        preflightContinue : true ,
         methods: ["POST", "GET"],
         // credentials: true
 }));
